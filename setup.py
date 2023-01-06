63391d177054b2e6eba626b061ba4d2c7513e014
Fahad Maqsood Qazi <50149692+fahadmaqsood@users.noreply.github.com>
2023-01-06 18:20:10 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pil_plus',
+    version='0.1',
+    license='APACHE 2.0',
+    author="Fahad Maqsood Qazi",
+    author_email='qazifahadmaqsood@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/fahadmaqsood/pil_plus',
+    keywords=['PIL', 'image processing', 'PIL wrapper', 'easy PIL'],
+    install_requires=[
+          'opencv-python',
+          'pillow',
+          'matplotlib',
+          'numpy',
+          'backgroundremover'
+      ],
+)
